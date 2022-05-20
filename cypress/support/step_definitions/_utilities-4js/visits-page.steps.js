f68143b2f2367bd834fb1aeef163000b4fff6dde
Craig Gregory <craig.gregory@NorthHighland.com>
2022-05-20 05:51:21 -0500

    Allow complex depth in TEST_CONFIGURATION

--- cypress/support/step_definitions/_utilities-4js/visits-page.steps.js
+++ cypress/support/step_definitions/_utilities-4js/visits-page.steps.js
@@ -5,7 +5,7 @@ Given('the user navagates to {string}', url => {
     const matched = /\${(.*)}/g.exec(url);
 
     if (matched)
-        url = Cypress.env('TEST_CONFIGURATION')[matched[1]];
+        url = eval("Cypress.env('TEST_CONFIGURATION')." + matched[1]);
 
     cy.visit(url);
 });
@@ -16,7 +16,7 @@ Then('the user sees the {string} page', title => {
     const matched = /\${(.*)}/g.exec(title);
 
     if (matched)
-        title = Cypress.env('TEST_CONFIGURATION')[matched[1]];
+        title = eval("Cypress.env('TEST_CONFIGURATION')." + matched[1]);
 
     cy.title().should('include', title);
 });
@@ -27,7 +27,7 @@ Then('the user sees {string} on the page', value => {
     const matched = /\${(.*)}/g.exec(value);
 
     if (matched)
-        value = Cypress.env('TEST_CONFIGURATION')[matched[1]];
+        value = eval("Cypress.env('TEST_CONFIGURATION')." + matched[1]);
 
     cy.contains(value);
 });
